030b2484facf3ef3f2e48a8d4ff3db88dfeabda7
Jonathan Fok Kan <jfokkan@gmail.com>
2019-12-27 12:07:10 -0500

    Added the wilks app

---
+++ gatsby-site/src/pages/wilks.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import Helmet from "react-helmet";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const WilksPage = () => {
+  useEffect(() => {
+      var script = document.createElement('script');
+      script.type = 'application/javascript';
+      script.src = '/wilks-app-init.js';
+      script.async = true;
+      document.body.appendChild(script);
+  });
+
+  return (
+    <Layout>
+      <SEO title="Wilks App" />
+      <h2>Wilks Score Calculator</h2>
+      <div id='wilks-app'></div>
+
+      <Helmet>
+        <script src="/wilks-elm-app.js"></script>
+      </Helmet>
+    </Layout>
+  );
+};
+
+export default WilksPage;
