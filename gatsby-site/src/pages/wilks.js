1be1a28b17c7dc027247eb535df49a419c743819
Jonathan Fok Kan <jfokkan@gmail.com>
2019-12-27 12:21:32 -0500

    Fixed warnings in imports

--- gatsby-site/src/pages/wilks.js
+++ gatsby-site/src/pages/wilks.js
@@ -6,18 +6,19 @@ import SEO from "../components/seo";
 
 const WilksPage = () => {
   useEffect(() => {
-      var script = document.createElement('script');
-      script.type = 'application/javascript';
-      script.src = '/wilks-app-init.js';
-      script.async = true;
-      document.body.appendChild(script);
+    var script = document.createElement("script");
+    script.type = "application/javascript";
+    script.src = "/wilks-app-init.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => script.remove();
   });
 
   return (
     <Layout>
       <SEO title="Wilks App" />
       <h2>Wilks Score Calculator</h2>
-      <div id='wilks-app'></div>
+      <div id="wilks-app"></div>
 
       <Helmet>
         <script src="/wilks-elm-app.js"></script>
