2b3a87a39851e8b58ddaecdb2da0db8f0e9bca57
Jonathan Fok Kan <jfokkan@gmail.com>
2019-12-27 12:14:34 -0500

    fixed format

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -6,13 +6,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-      {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-              name: `images`,
-              path: `${__dirname}/content/images`,
-          },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/content/images`,
       },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
